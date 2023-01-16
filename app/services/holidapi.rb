7eb1d1c91e8f85f1341f17ff54159ec56121941c
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-16 11:37:59 -0700

    feat: Initial api call service integration

---
+++ app/services/holidapi.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+require 'pry'
+require 'json'
+
+class Holidapi
+  def initialize
+    @holidays = get('https://date.nager.at/api/v3/NextPublicHolidays/US')
+  end
+
+  def get(url)
+    response = HTTParty.get(url)
+    parse(response)
+  end
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def next(num)
+    num -= 1
+    @holidays[0..num]
+  end
+end
+
