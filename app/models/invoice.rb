31da162a73e48e9e8710dffd09f86e3725e665a8
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-17 12:22:39 -0700

    feat: Views for total discounted revenue method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -15,7 +15,21 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
-  def total_revenue_after_discounts
-    #us6
+  # def total_revenue_after_discounts
+  #   invoice_items
+  #   #us6
+  # end
+
+  def total_revenue_after_discounts_ruby #us6
+    sum = 0
+    invoice_items.each do |ii|
+      total = ii.unit_price * ii.quantity
+      if ii.find_discount.nil?
+        sum += total
+      else
+        sum += total - (total * ii.find_discount.discount.to_f / 100)
+      end
+    end
+    sum
   end
 end
