193af4d5bb5b1bea70d6fe2c272728a4003c3099
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-17 13:07:00 -0700

    fix: Refactor total_revenue_after_discounts method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -15,21 +15,14 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
-  # def total_revenue_after_discounts
-  #   invoice_items
-  #   #us6
-  # end
-
   def total_revenue_after_discounts_ruby #us6
-    sum = 0
-    invoice_items.each do |ii|
+    invoice_items.map do |ii|
       total = ii.unit_price * ii.quantity
       if ii.find_discount.nil?
-        sum += total
+        total
       else
-        sum += total - (total * ii.find_discount.discount.to_f / 100)
+        total * (100 - ii.find_discount.discount.to_f) / 100
       end
-    end
-    sum
+    end.sum
   end
 end
